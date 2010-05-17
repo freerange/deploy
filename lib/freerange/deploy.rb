a829f64bec51e306549209db1264c92dfbe439a9
Tom Ward <tom@popdog.net>
2010-05-17 16:46:46 +0100

    Removed machine setup tasks (monit, redis, hosts, etc) in favour of using freerange-puppet

--- lib/freerange/deploy.rb
+++ lib/freerange/deploy.rb
@@ -2,8 +2,8 @@ require 'capistrano/ext/multistage'
 
 Capistrano::Configuration.instance(:must_exist).load do
   # User details
-  set :user,          'deploy'
-  set :group,         'admin'
+  set :user,          'freerange'
+  set :group,         'freerange'
 
   # Application details
   set(:runner)        { user }
@@ -21,106 +21,6 @@ Capistrano::Configuration.instance(:must_exist).load do
   default_run_options[:pty]     = true # needed for git password prompts
   ssh_options[:forward_agent]   = true # use the keys for the person running the cap command to check out the app
 
-  set(:vhost_template) {<<-EOT
-<VirtualHost *:80>
-ServerName #{domain}
-DocumentRoot /var/www/#{application}/current/public
-<Directory "/var/www/#{application}/current/public">
-allow from all
-Options +Indexes
-</Directory>
-</VirtualHost>
-EOT
-  }
-
-  namespace :host do
-    task :create do
-      put vhost_template.strip, "/etc/apache2/sites-available/#{domain}"
-    end
-
-    task :enable do
-      run "a2ensite #{domain}"
-      sudo "apache2ctl graceful"
-    end
-
-    task :disable do
-      run "a2dissite #{domain}"
-      sudo "apache2ctl graceful"
-    end
-
-    task :setup do
-      host.create
-      host.enable
-    end
-  end
-
-  namespace :redis do
-    task :setup do
-      if fetch('require_redis', nil)
-        sudo "apt-get install redis-server"
-      end
-    end
-  end
-
-  after "deploy:setup", "host:setup"
-  after "deploy:setup", "redis:setup"
-
-  set :monit_processes, []
-
-  def monit_process(name, directives)
-    monit_processes << "check process #{name}#{directives}"
-  end
-
-  def combined_monit_config
-    base = %{
-set daemon  120
-  with start delay 240
-set logfile syslog facility log_daemon
-set mailserver localhost
-set mail-format { from: #{monit_email} }
-set alert #{monit_email}
-set httpd port 2812 and
-  allow deploy:fr33r4n63
-check system localhost
-  if loadavg (1min) > 4 then alert
-  if loadavg (5min) > 2 then alert
-  if memory usage > 75% then alert
-  if cpu usage (user) > 70% then alert
-  if cpu usage (system) > 30% then alert
-  if cpu usage (wait) > 20% then alert}
-
-    apache = %{
-check process apache2 with pidfile /var/run/apache2.pid
-  start program = "/usr/sbin/apache2ctl start"
-  stop  program = "/usr/sbin/apache2ctl stop"
-  if cpu > 60% for 2 cycles then alert
-  if cpu > 80% for 5 cycles then restart
-  if children > 200 then restart
-  if loadavg(5min) greater than 10 for 8 cycles then restart}
-
-    ([base, apache] + monit_processes).join("\n")
-  end
-
-  def sudo_put(data, target)
-    tmp = "#{shared_path}/~tmp-#{rand(9999999)}"
-    put data, tmp
-    on_rollback { run "rm #{tmp}" }
-    sudo "cp -f #{tmp} #{target} && rm #{tmp}"
-  end
-
-  namespace :monit do
-    task :setup do
-      if monit_processes.any?
-        sudo "apt-get install monit"
-        sudo_put combined_monit_config, "/etc/monit/monitrc"
-        sudo_put "startup=1", "/etc/default/monit"
-        sudo "/etc/init.d/monit restart"
-      end
-    end
-  end
-
-  after "deploy:setup", "monit:setup"
-
   # We're using passenger, so start/stop don't apply, while restart needs to just
   # touch path/restart.txt
 
@@ -133,6 +33,12 @@ check process apache2 with pidfile /var/run/apache2.pid
 
     task :bundle, :roles => :app do
       run "cd #{release_path} && bundle install"
+    end
+    
+    desc "Does a full deploy, including setup.  For lazy people who don't like to type cap deploy:setup deploy"
+    task :full do
+      deploy.setup
+      deploy.default
     end
 
     task :announce do
