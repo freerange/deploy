e0bc8e1982687069ebf4c1083255432c2eed0fab
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2010-11-16 15:28:15 +0000

    Ensure we actually catch the exception
    The `rescue => e` syntax only rescues for subclasses of `StandardError`, but the exception that is raised is a `Timeout::Error`, which isn't a subclass.

--- lib/freerange/webhook.rb
+++ lib/freerange/webhook.rb
@@ -7,7 +7,7 @@ module Freerange
     def self.post(url, data)
       begin
         Net::HTTP.post_form(URI.parse(url),{"payload" => data.to_json})
-      rescue => e
+      rescue Exception => e
         puts "ERROR posting to deploy_webhook_url = #{url} #{e}"
       end
     end
